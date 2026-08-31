975e4534b6aa0f974ecba68afca716ebe2eab03f
Omm Narayan Sahu <ommnarayansahu07@gmail.com>
2026-09-01 00:27:21 +0530

    Create Q3a.cpp

---
+++ LAB-2/Q3a.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Pattern
+{
+public:
+    void display()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                cout << j << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main()
+{
+    Pattern obj;
+    obj.display();
+
+    return 0;
+}
