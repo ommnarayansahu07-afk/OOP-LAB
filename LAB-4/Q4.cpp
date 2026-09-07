4621b0b41786b934a7b50b8ef712538474131b77
Omm Narayan Sahu <ommnarayansahu07@gmail.com>
2026-09-07 22:40:23 +0530

    Refactor Fibonacci class to include user input

---
+++ LAB-4/Q4.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Fibonacci
+{
+    int n;
+
+public:
+   
+    Fibonacci()
+    {
+        n = 0;
+    }
+
+    void input()
+    {
+        cout << "Enter number of terms: ";
+        cin >> n;
+    }
+
+    void generate()
+    {
+        int a = 0, b = 1, c;
+
+        cout << "Fibonacci Series: ";
+
+        for (int i = 0; i < n; i++)
+        {
+            cout << a << " ";
+
+            c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+};
+
+int main()
+{
+    Fibonacci f;   
+
+    f.input();
+    f.generate();
+
+    return 0;
+}
